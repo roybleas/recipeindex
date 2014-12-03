fc6ea314efa8a1419b41720e563c8641016ef2c1
roybleas <roybleas@ozemail.com.au>
2014-12-04 10:28:36 +1100

    converted contoller from user to users

--- spec/routing/user_routes_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "routes to the user controller" do
-  it "routes a sign up route" do
-    expect(:get => signup_path).
-      to route_to(:controller => "user", :action => "new")
-  end
-end
