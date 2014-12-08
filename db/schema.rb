6ef0abb7bbd55b017906c38b514f7c5682aeab69
roybleas <roybleas@ozemail.com.au>
2014-12-08 13:45:57 +1100

    add checkbox remember me to login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201064012) do
+ActiveRecord::Schema.define(version: 20141208022417) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141201064012) do
     t.datetime "updated_at"
     t.string   "screen_name"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
