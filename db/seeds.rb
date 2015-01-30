c0a1b48c1f33577f11e58fac2d722004e55ef3e9
roybleas <roybleas@ozemail.com.au>
2015-01-30 17:10:25 +1100

    Add test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "Roy User",
+             screen_name: "Roy",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+             
+99.times do |n|
+  
+  screen_name = Faker::Name.first_name
+  name  = Faker::Name.name
+  password = "password"
+  User.create!(name:  name,
+               screen_name: screen_name,
+               password:              password,
+               password_confirmation: password)
+end
