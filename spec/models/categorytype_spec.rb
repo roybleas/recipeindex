6c9cd9d66f7e19d679330f1cf019ae81aae38646
roybleas <roybleas@ozemail.com.au>
2015-04-29 10:28:25 +1000

    add model tests

--- spec/models/categorytype_spec.rb
+++ spec/models/categorytype_spec.rb
@@ -1,5 +1,31 @@
+# == Schema Information
+#
+# Table name: categorytypes
+#
+#  id         :integer          not null, primary key
+#  code       :string(255)
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe Categorytype, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a code and name" do
+  	cattype = Categorytype.new(code: "A", name: "a category type name")
+  	expect(cattype).to be_valid
+  end
+  
+  it "is invalid without a code" do
+  	cattype = Categorytype.new(code: nil, name: "a category type name")
+  	cattype.valid?
+  	expect(cattype.errors[:code]).to include("can't be blank")
+  end 
+  
+  it "is invalid without a name" do
+  	cattype = Categorytype.new(code: "A", name: nil)
+  	cattype.valid?
+  	expect(cattype.errors[:name]).to include("can't be blank")
+  end 
 end
