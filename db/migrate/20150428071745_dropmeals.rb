a972becd91cedb9d6afc20a17e459d00740cd30b
roybleas <roybleas@ozemail.com.au>
2015-05-22 14:57:06 +1000

    setup for Heroku

--- db/migrate/20150428071745_dropmeals.rb
+++ db/migrate/20150428071745_dropmeals.rb
@@ -1,13 +1,13 @@
 class Dropmeals < ActiveRecord::Migration
-  def up
-    drop_table :meals
-  end
+#  def up
+#    drop_table :meals
+#  end
 
-  def down
-    create_table :meals do |t|
-      t.string :description
-      t.integer :seq
-      t.timestamps
-    end
-  end
+#  def down
+#    create_table :meals do |t|
+#      t.string :description
+#      t.integer :seq
+#      t.timestamps
+#    end
+#  end
 end
