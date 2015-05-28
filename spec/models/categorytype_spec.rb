70d88fd5cc175e48420dd3b7060e49cbadb01560
roybleas <roybleas@ozemail.com.au>
2015-05-28 14:01:03 +1000

    set to use Factorygirl to create records

--- spec/models/categorytype_spec.rb
+++ spec/models/categorytype_spec.rb
@@ -13,35 +13,35 @@ require 'rails_helper'
 
 RSpec.describe Categorytype, :type => :model do
   it "is valid with a code and name" do
-  	cattype = Categorytype.new(code: "A", name: "a category type name")
+  	cattype = create(:categorytype)
   	expect(cattype).to be_valid
   end
   
   it "is invalid without a code" do
-  	cattype = Categorytype.new(code: nil, name: "a category type name")
+  	cattype = build(:categorytype, code: nil)
   	cattype.valid?
   	expect(cattype.errors[:code]).to include("can't be blank")
   end 
   
   it "is invalid without a name" do
-  	cattype = Categorytype.new(code: "A", name: nil)
+  	cattype = build(:categorytype, name:nil)
   	cattype.valid?
   	expect(cattype.errors[:name]).to include("can't be blank")
   end 
   
   it "is invalid with a duplicate code" do
-  	cattype = Categorytype.create(code: "A", name: "a category type name")
+  	cattype = create(:categorytype)
   	expect(cattype).to be_valid
-  	cattype = Categorytype.new(code: "A", name: "another category type name")
-  	cattype.valid?
-  	expect(cattype.errors[:code]).to include("has already been taken")
+  	cattype2 = build(:categorytype, code: cattype.code)
+  	cattype2.valid?
+  	expect(cattype2.errors[:code]).to include("has already been taken")
   end 
   
-  it "is invalid with a duplicate code" do
-  	cattype = Categorytype.create(code: "A", name: "a category type name")
+  it "is invalid with a duplicate name" do
+  	cattype = create(:categorytype)
   	expect(cattype).to be_valid
-  	cattype = Categorytype.new(code: "B", name: "a category type name")
-  	cattype.valid?
-  	expect(cattype.errors[:name]).to include("has already been taken")
+  	cattype2 = build(:categorytype, name: cattype.name)
+  	cattype2.valid?
+  	expect(cattype2.errors[:name]).to include("has already been taken")
   end 
 end
