493936d202760f952914f4e2649d0c81d8989264
roybleas <roybleas@ozemail.com.au>
2015-06-17 14:28:13 +1000

    add issues to view

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,4 +1,22 @@
 module IssuesHelper
 
+class CurrentYear
+	attr :current_year
+	def initialize()
+		@current_year = -1
+	end
 	
+	def add_new_row_on_change_of_year(this_year)
+		html_txt = ""
+		
+		if this_year != @current_year
+			html_txt = " </tr>\n" unless @current_year == -1
+			html_txt = html_txt + "<tr><th> #{this_year}</th> "
+			@current_year = this_year
+		end
+		return html_txt.html_safe
+	end
+end
+
+		
 end
